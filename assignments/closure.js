5eb2cecd2e01cd5e9b7d5199b4bc90f980064f8b
Brandon Nichols <brandonaronnichols@gamil.com>
2019-05-24 21:11:35 -0500

    Finished stretch goals for closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -35,7 +35,29 @@ newCounter();
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let count = 0;
+  function process(){
+    const countObj = {
+      increment: function(){
+        count++;
+        console.log(count);
+      },
+      decrement: function(){
+        count--;
+        console.log(count);
+      }
+    }
+    return countObj;
+  }
+
+  return process;
+  
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+let counting = counterFactory();
+
+counting().increment();
+counting().decrement();
